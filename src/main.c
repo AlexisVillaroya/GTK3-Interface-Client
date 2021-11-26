19d8ae0778df80c26db7c21c1abb5627ca27ef1d
AlexisVillaroya <alexisvillaroya2a@gmail.com>
2021-11-26 11:23:32 +0100

    remove client connexion

--- src/main.c
+++ src/main.c
@@ -1,9 +1,7 @@
-#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "client_connection/client_connection.h"
 #include "client_interface/client_interface.h"
 
 int main(int argc, char** argv) {
